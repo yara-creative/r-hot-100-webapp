feca54aa1a5f2e143783ef9a3f9f97a5723db459
yara-creative <95034236+yara-creative@users.noreply.github.com>
2023-01-09 19:02:57 +0100

    today's data

--- youtube_links.py
+++ youtube_links.py
@@ -1,4 +1,4 @@
-youtube_embed_src1="https://www.youtube.com/embed/videoseries?list=TLGGZdFV4rGFTKQwNzAxMjAyMw"
-youtube_embed_src2="https://www.youtube.com/embed/videoseries?list=TLGGtJZF2DpNa2YwNzAxMjAyMw"
-playlist_link_1="https://www.youtube.com/watch?v=c18441Eh_WE&list=TLGGZdFV4rGFTKQwNzAxMjAyMw"
-playlist_link_2="https://www.youtube.com/watch?v=X8UR2TFUp8w&list=TLGGtJZF2DpNa2YwNzAxMjAyMw"
+youtube_embed_src1="https://www.youtube.com/embed/videoseries?list=TLGGev5hYL77SokwOTAxMjAyMw"
+youtube_embed_src2="https://www.youtube.com/embed/videoseries?list=TLGGF3LIEhc8v-8wOTAxMjAyMw"
+playlist_link_1="https://www.youtube.com/watch?v=ZY4ywyFXdik&list=TLGGev5hYL77SokwOTAxMjAyMw"
+playlist_link_2="https://www.youtube.com/watch?v=yHu3f7ThNEQ&list=TLGGF3LIEhc8v-8wOTAxMjAyMw"
